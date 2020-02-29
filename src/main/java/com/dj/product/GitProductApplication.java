c7f62acad3e30a5701bb633c4bb52a91006c5072
lxf <lxf961101@163.com>
2020-02-29 22:51:56 +0800

    init commit

---
+++ src/main/java/com/dj/product/GitProductApplication.java
@@ -0,0 +1,13 @@
+package com.dj.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitProductApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dj/product/GitProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dj.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
